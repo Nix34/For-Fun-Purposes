2ef95b6cf3378dffdc0a098dd052f483b624450a
Nix <nicolo.developer34@gmail.com>
2023-04-27 13:36:58 +0800

    Info component

---
+++ src/components/pages/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Info from "../Info";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <Info />
+    </div>
+  );
+}
+
+export default App;
